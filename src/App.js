45b7a8268baf87ac5b631652612571d770157d59
urvish <sojitraurvish0@gmail.com>
2024-01-25 19:40:42 +0530

    remove bugs in routing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route index path='/products' element={<Products/>}/>
-        <Route index path='/product/:id' element={<Product/>}/>
+        <Route path='/product/:id' element={<Product/>}/>
       </Routes>
     </div>
   );
